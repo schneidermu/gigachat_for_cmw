0caad6fcb6f75a378f12dfdb36e9b696742e365f
schneidermu <schneider.mark14@gmail.com>
2024-07-05 12:42:30 +0300

    Docker deploy

--- app/server.py
+++ app/server.py
@@ -19,4 +19,4 @@ class GigaChatResponse(Resource):
 
 api.add_resource(GigaChatResponse, "/gigachat", "/gigachat/")
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
