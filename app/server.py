c251e77f5721eefca1dc3c662312b154fc91c5bf
alexeybur <schneider.mark14@gmail.com>
2024-08-16 10:19:04 +0700

    remove writing in file

--- app/server.py
+++ app/server.py
@@ -11,12 +11,6 @@ class ModelResponse(Resource):
     def get(self, query: str=""):
         query = request.args.get('query')
         answer = get_answer(query)
-        with open("output.txt", "w") as file:
-            file.write(f"Запрос: {query}\n")
-            file.write(f"Ответ: {answer}\n")
-        with open("output.bin", "wb") as binary_file:
-            binary_file.write(f"Запрос: {query}\n".encode('utf-8'))
-            binary_file.write(f"Ответ: {answer}\n".encode('utf-8'))
         return {
             "answer": answer
         }, 200
