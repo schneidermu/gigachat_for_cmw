568ea6717c2ae73c205bebd7917aea2ac8f3bc09
alexeybur <schneider.mark14@gmail.com>
2024-09-09 16:52:05 +0300

    Document QA

--- app/server.py
+++ app/server.py
@@ -16,6 +16,22 @@ class ModelResponse(Resource):
         }, 200
 
 
+class FileResponse(Resource):
+    def get(self, query: str=""):
+        question = request.args.get('question')
+        content = request.args.get('content')
+
+        # content = decode(content)
+
+        query = f'Ты отвечаешь на вопросы по документам. Вот текст документа:\n\n{content}\n\nОтветь на вопрос:\n\n{question}\n\nОтвет:\n\n'
+        answer = get_answer(query)
+        return {
+            "answer": answer
+        }, 200
+
+
 api.add_resource(ModelResponse, "/model", "/model/")
+api.add_resource(FileResponse, "/file", "/file/") # сюда запрос отправляем для ответа на вопрос по документу
+
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
